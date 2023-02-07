0f7acb02943f530208f23cfbb2258c9658511299
sdasd30 <danielzhao2@gmail.com>
2023-02-07 18:38:01 -0500

    Scene reloading script. Not attached to anything

--- Assets/Scripts/Game/ReloadScene.cs
+++ Assets/Scripts/Game/ReloadScene.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Reload : MonoBehaviour
+public class ReloadScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,14 @@ public class Reload : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxisRaw("Reload") > .5f)
+        {
+            SceneReload();
+        }
+    }
+
+    public void SceneReload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
     }
 }
