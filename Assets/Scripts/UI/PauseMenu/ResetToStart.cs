56df42a15ad427fbc5445a7a3ca144c03fc1239e
Daniel Zhao <danielzhao2@gmail.com>
2023-02-22 15:41:14 -0500

    Fixed game being paused

--- Assets/Scripts/UI/PauseMenu/ResetToStart.cs
+++ Assets/Scripts/UI/PauseMenu/ResetToStart.cs
@@ -7,6 +7,7 @@ public class ResetToStart : MonoBehaviour
 {
     public void LetsGo()
     {
+        Time.timeScale = 1.0f;
         SceneManager.LoadScene("w0_s1", LoadSceneMode.Single);
     }
 }
