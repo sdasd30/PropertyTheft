67d3dddb271da8b422872880bb885507f4d6656a
Kimsa <winstonding88@gmail.com>
2023-02-23 16:29:25 -0600

    Developing UI

---
+++ Assets/TopBarUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopBarUI : MonoBehaviour
+{
+
+    public GameObject player_game_object;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
